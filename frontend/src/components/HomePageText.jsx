2b0fdfe542c4ff89bc562b9a09c1219827ab08a0
Citronskal <yiaswift@gmail.com>
2024-06-06 21:39:00 +0200

    trying to render out the text and btn bg color

--- frontend/src/components/HomePageText.jsx
+++ frontend/src/components/HomePageText.jsx
@@ -2,24 +2,55 @@
 // HomePageText.jsx
 import { Link } from "react-router-dom";
 
-export const HomePageText = ({ textTitle, text, textImg, textImgAlt, linkTo, btnName, titleStyle }) => {
+export const HomePageText = ({
+  textTitle,
+  text,
+  textImg,
+  textImgAlt,
+  linkTo,
+  btnName,
+  textBg,
+  btnBg,
+  btnText,
+  h2,
+  body,
+  imageFirst,
+}) => {
+  // Debug logs to check if values are correctly passed
+  console.log("textBg:", textBg);
+  
   return (
-    <div className="flex flex-col items-center md:flex-row md:items-start">
-      <div className="md:w-1/2 p-4">
-        <h4 className={titleStyle}>{textTitle}</h4>
-        <p className="mb-4">{text}</p>
-        <div>
-          <Link to={linkTo}>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <div
+      className={`relative flex flex-col ${
+        imageFirst ? "md:flex-row" : "md:flex-row-reverse"
+      } items-center text-center bg-cover bg-center `}
+    >
+      <img
+        src={textImg}
+        alt={textImgAlt}
+        className="w-full md:w-2/3 h-auto md:h-screen object-cover"
+      />
+      <div
+        className={`relative z-10 p-6 max-w-2xl mx-auto ${textBg} rounded-lg`}
+      >
+        <h2
+          className={`lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir mb-1.8rem text-left ${h2}`}
+        >
+          {textTitle}
+        </h2>
+        <p
+          className={`text-left lg:text-textlg md:text-textmd sm:text-textsm font-avenir ${body}`}
+        >
+          {text}
+        </p>
+        <Link to={linkTo}>
+          <div className="inline-block">
+            <button className={`py-2 px-4 rounded ${btnBg} ${btnText}`}>
               {btnName}
             </button>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
-      {textImg && (
-        <img src={textImg} alt={textImgAlt} className="md:w-1/2 h-auto" />
-      )}
     </div>
   );
 };
-
