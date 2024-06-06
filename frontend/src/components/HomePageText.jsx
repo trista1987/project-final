4267ba906d713d7afa57f16c6eca7d347d3fe0eb
Citronskal <yiaswift@gmail.com>
2024-06-06 17:20:35 +0200

    Header completed

--- frontend/src/components/HomePageText.jsx
+++ frontend/src/components/HomePageText.jsx
@@ -1,18 +1,25 @@
 // import { VisitBtn } from "./buttons/VisitBtn";
-import {Link} from "react-router-dom"
-export const HomePageText = ({ textTitle, text, textImg, textImgAlt, linkTo, btnName, titleStyle}) => {
+// HomePageText.jsx
+import { Link } from "react-router-dom";
+
+export const HomePageText = ({ textTitle, text, textImg, textImgAlt, linkTo, btnName, titleStyle }) => {
   return (
-    <>
-      <div>
+    <div className="flex flex-col items-center md:flex-row md:items-start">
+      <div className="md:w-1/2 p-4">
         <h4 className={titleStyle}>{textTitle}</h4>
-        <p>{text}</p>
+        <p className="mb-4">{text}</p>
         <div>
-            <Link to={linkTo}>
-                <button>{btnName}</button>
-            </Link>
+          <Link to={linkTo}>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              {btnName}
+            </button>
+          </Link>
         </div>
       </div>
-      <img src={textImg} alt={textImgAlt} />
-    </>
+      {textImg && (
+        <img src={textImg} alt={textImgAlt} className="md:w-1/2 h-auto" />
+      )}
+    </div>
   );
 };
+
