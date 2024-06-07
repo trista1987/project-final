531d931042e2dd760ca4032866a49a82c4b4ce25
Citronskal <yiaswift@gmail.com>
2024-06-07 21:17:48 +0200

    HomePage-Finland completed

--- frontend/src/components/HomePageText.jsx
+++ frontend/src/components/HomePageText.jsx
@@ -1,66 +1,3 @@
-// // import { VisitBtn } from "./buttons/VisitBtn";
-// // HomePageText.jsx
-// import { Link } from "react-router-dom";
-
-// export const HomePageText = ({
-//   textTitle,
-//   text,
-//   textImg,
-//   textImgAlt,
-//   linkTo,
-//   btnName,
-//   textBg,
-//   btnBg,
-//   btnText,
-//   h2,
-//   body,
-//   imageFirst,
-//   textPosition,
-// }) => {
-//   // To check if all dynamic values are correctly passed
-//   console.log("textBg:", textBg);
-//   console.log("btnBg:", btnBg);
-//   console.log("btnText:", btnText);
-
-
-//   return (
-//     <div
-//       className={`relative flex flex-col ${
-//         imageFirst ? "md:flex-row" : "md:flex-row-reverse"
-//       } items-center text-center bg-cover bg-center `}
-//     >
-//       <img
-//         src={textImg}
-//         alt={textImgAlt}
-//         className="w-full md:w-2/3 h-auto object-cover"
-//       />
-//       <div
-//         className={`lg:relative md: p-6 mx-auto ${textBg} rounded-lg w-full lg:w-[535px] h-auto lg:h-[477px]`}
- 
-//       >
-//         <h2
-//           className={`lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir mb-1.8rem text-left ${h2}`}
-//         >
-//           {textTitle}
-//         </h2>
-//         <p
-//           className={`text-left lg:text-textlg md:text-textmd sm:text-textsm font-avenir ${body}`}
-//         >
-//           {text}
-//         </p>
-//         <Link to={linkTo}>
-//           <div className="inline-block">
-//             <button className={`py-2 px-4 rounded ${btnBg} ${btnText}`}>
-//               {btnName}
-//             </button>
-//           </div>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-import React from "react";
 import { Link } from "react-router-dom";
 
 export const HomePageText = ({
@@ -87,16 +24,16 @@ export const HomePageText = ({
     <div
       className={`relative flex flex-col ${
         imageFirst ? "md:flex-row" : "md:flex-row-reverse"
-      } items-center text-center bg-cover bg-center my-10 mb-10`}
+      } items-center text-center bg-cover bg-center my-10`}
     >
       <div className="relative w-full md:w-2/3">
         <img
           src={textImg}
           alt={textImgAlt}
-          className="w-full h-auto object-cover"
+          className="lg:w-full h-[700px] object-cover"
         />
         <div
-          className={`absolute p-6 ${textBg} rounded-lg w-full lg:w-[535px] h-auto lg:h-[477px] z-10 ${
+          className={`absolute p-6 ${textBg} rounded-lg w-full lg:w-[535px] h-[400px] z-10 ${
             imageFirst
               ? "bottom-10 lg:bottom-auto lg:left-[670px] lg:top-1/4"
               : "bottom-10 lg:bottom-auto lg:right-[670px] lg:top-1/4"
@@ -113,8 +50,8 @@ export const HomePageText = ({
             {text}
           </p>
           <Link to={linkTo}>
-            <div className="inline-block">
-              <button className={`py-2 px-4 rounded ${btnBg} ${btnText}`}>
+            <div className={`inline-block ${btnBg}`}>
+              <button className={`py-2 px-4 rounded  ${btnText}`}>
                 {btnName}
               </button>
             </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { HomePageText } from "../components/HomePageText";
 import { PageHeader } from "../components/PageHeader";
 import { ScrollToTop } from "../components/buttons/ToTopBtn";
 import { Footer } from "../components/Footer";
-import Homepagetext from "../components/Homepagetext.json";
+import { HomePageFi } from "../components/HomePageFi"
 
 export const HomePage = () => {
   return (
@@ -10,26 +10,7 @@ export const HomePage = () => {
       <PageHeader placeName={"Scandinavia"} />
       {/* <HomePageText textTitle={"Find your inner Finn in Nature | Explore Finland"} text={"The Helsinki Happiness hacks are your gateway to mastering your happiness and taking a step closer to find your inner Finn"} textImg={""} textImgAlt={""} linkTo={"/finland"} btnName={"Visit Finland"} titleStyle={""}/>
       <HomePageText textTitle={"Experience the season | Explore Sweden"} text={"See the magnificant season change through the eyes of reindeer"} textImg={""} textImgAlt={""} linkTo={"/sweden"} btnName={"Visit Sweden"}/> */}
-      <main className="mt-10">
-        {Homepagetext.homepagetext.map((section, index) => (
-          <section key={index}>
-            <HomePageText
-              textTitle={section.textTitle}
-              text={section.text}
-              textImg={section.textImg}
-              textImgAlt={section.textImgAlt}
-              linkTo={section.linkTo}
-              btnName={section.btnName}
-              textBg={section.textBg}
-              btnBg={section.btnBg}
-              btnText={section.btnText}
-              h2={section.h2}
-              body={section.body}
-              imageFirst={section.imageFirst}
-            />
-          </section>
-        ))}
-      </main>
+      <HomePageFi />
       <ScrollToTop />
       <Footer />
     </>
