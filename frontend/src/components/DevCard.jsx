185cfa992d4a138360903d96ca910cefdbfdc64e
Citronskal <yiaswift@gmail.com>
2024-06-11 23:09:06 +0200

    devcard adjusted

--- frontend/src/components/DevCard.jsx
+++ frontend/src/components/DevCard.jsx
@@ -1,33 +1,85 @@
-import {Github} from "./iconFolder/Github"
-import {Linkedin} from "./iconFolder/Linkedin"
-import {Portfolio} from "./iconFolder/Portfolio"
+import { Github } from "./iconFolder/Github";
+import { Linkedin } from "./iconFolder/Linkedin";
+import { Portfolio } from "./iconFolder/Portfolio";
 
-
-export const DevCard = ({ devImage, devName, developerName, devIntro, gitLink, linkedinLink, portfolioLink}) => {
+export const DevCard = ({
+  devImage,
+  devName,
+  developerName,
+  devIntro,
+  gitLink,
+  linkedinLink,
+  portfolioLink,
+}) => {
   return (
-    <>
-      <div className="flex flex-col items-center">
-        <img
-          src={devImage}
-          alt={devName}
-          className="w-[300px] h-[300px] object-cover rounded-full"
-        />
+    <div className="flex md:flex-row flex-col items-center space-x-0 md:space-x-6 space-y-4 md:space-y-0">
+      <img
+        src={devImage}
+        alt={devName}
+        className="md:w-[300px] md:h-[300px] sm:w-[150px] sm:h-[150px] object-cover rounded-full flex-none"
+      />
+      <div className="flex-1 md:ml-[60px]">
+        <h4 className="lg:text-logoLg md:text-logoMd sm:text-logoSm font-semibold text-fontColor font-avenir text-center">
+          {developerName}
+        </h4>
+        <p className="lg:text-textlg md:text-textmd sm:text-textsm font-normal text-fontColor">
+          {devIntro}
+        </p>
+        <div className="flex justify-center md:justify-start space-x-4 mt-2 ">
+          <a href={gitLink} className="text-gray-600 hover:text-black">
+            <Github />
+          </a>
+          <a href={linkedinLink} className="text-gray-600 hover:text-black">
+            <Linkedin />
+          </a>
+          <a href={portfolioLink} className="text-gray-600 hover:text-black">
+            <Portfolio />
+          </a>
+        </div>
       </div>
-      <div className="flex justify-center">
-        <h4>{developerName}</h4>
-        <p>{devIntro}</p>
-      </div>
-      <div className="flex justify-center space-x-4 mt-[16px]">
-        <a href={gitLink} className="text-gray-600 hover:text-bg2">
-          <Github />
-        </a>
-        <a href={linkedinLink} className="text-gray-600 hover:text-black">
-          <Linkedin />
-        </a>
-        <a href={portfolioLink} className="text-gray-600 hover:text-black">
-          <Portfolio />
-        </a>
-      </div>
-    </>
+    </div>
   );
 };
+
+// import { Github } from "./iconFolder/Github";
+// import { Linkedin } from "./iconFolder/Linkedin";
+// import { Portfolio } from "./iconFolder/Portfolio";
+
+// export const DevCard = ({
+//   devImage,
+//   devName,
+//   developerName,
+//   devIntro,
+//   gitLink,
+//   linkedinLink,
+//   portfolioLink,
+// }) => {
+//   return (
+//     <div className="flex items-center space-x-6">
+//       <img
+//         src={devImage}
+//         alt={devName}
+//         className="md:w-[300px] md:h-[300px] object-cover rounded-full flex-none sm:w-[150px] sm:h-[150px]"
+//       />
+//       <div className="flex-1 md:ml-[20px] ">
+//         <h4 className="lg:text-textlg md:text-textmd sm:text-textsm font-avenir text-fontColor">
+//           {developerName}
+//         </h4>
+//         <p className="lg:text-textlg md:text-textmd sm:text-textsm font-avenir text-fontColor">
+//           {devIntro}
+//         </p>
+//         <div className="flex justify-start space-x-4 mt-2">
+//           <a href={gitLink} className="text-gray-600 hover:text-black">
+//             <Github />
+//           </a>
+//           <a href={linkedinLink} className="text-gray-600 hover:text-black">
+//             <Linkedin />
+//           </a>
+//           <a href={portfolioLink} className="text-gray-600 hover:text-black">
+//             <Portfolio />
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -23,7 +23,7 @@ export const AboutPage = () => {
           className="md:h-[580px] w-[100%]"
         />
         <div className="absolute top-1/4 md:ml-[50px] sm:ml-[10px]">
-          <h2 className="lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir text-cardBg font-semibold">
+          <h2 className="lg:text-h1lg md:text-h1md sm:text-h1sm font-avenir text-cardBg font-semibold">
             Hello👋
             <br />
             It's Trista and Yia
@@ -40,7 +40,7 @@ export const AboutPage = () => {
         <h2 className="lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir">
           About us
         </h2>
-        <p className="font-avenir lg:text-textlg md:text-textmd sm:text-textsm md:w-[700px] items-end sm:w-[300px]">
+        <p className="font-avenir lg:text-textlg md:text-textmd sm:text-textsm md:w-[700px] items-end sm:w-[300px] mb-[50px]">
           We both have studied and worked in Sweden and Finland for almost five
           years. Coming from similar cultural background, we found many
           similarities in our Nordic journey. But one thing we both agree is the
@@ -60,7 +60,7 @@ export const AboutPage = () => {
           className="md:w-[640px] h-auto object-cover sm:w-[300px]"
         />
       </div>
-      <div className="w-full justify-center py-[20px]">
+      <div className="flex flex-col items-center justify-center my-[30px] md:py-[40px]">
         <DevCard
           devImage={"/developerImages/yia.png"}
           devName={"Yia"}
@@ -71,7 +71,7 @@ export const AboutPage = () => {
           portfolioLink={"https://yia-porfolio.netlify.app/"}
         />
       </div>
-      <div className="w-full justify-center py-[20px]">
+      <div className="flex flex-col items-center justify-center my-[30px] md:py-[40px]">
         <DevCard
           devImage={"/developerImages/trista.jpeg"}
           devName={"Trista"}
