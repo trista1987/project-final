f01a7b19a9eba55945607eddfc76ddcc44bb69ac
Citronskal <yiaswift@gmail.com>
2024-06-12 21:49:45 +0200

    padding changed finland card

--- frontend/src/components/HomePageFinland.jsx
+++ frontend/src/components/HomePageFinland.jsx
@@ -13,7 +13,10 @@ export const HomePageFinland = () => {
     <div className="relative w-full md:w-2/3 h-[600px] flex flex-col md:flex-row sm:flex-wrap">
       <div className="flex-1 order-2 md:order-1">
         <div className="z-10 relative md:absolute md:top-1/2 md:left-1/2 transform md:-translate-x-2/4 md:-translate-y-1/2 bg-bg1 md:rounded-lg flex flex-col justify-center md:p-12">
-          <h2 className="lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir text-fontColor md:p-[16px] md:mt-[8px]">
+          <h2
+            className="lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir text-fontColor md:p-[16px] md:mt-[8px]"
+            style={{ margin: "16px", paddingTop: "8px" }}
+          >
             {textTitle}
           </h2>
           <p
