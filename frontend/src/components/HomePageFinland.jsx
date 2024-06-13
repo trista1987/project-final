b81efa24fe30d2b667b856655d024923e81ee82d
Citronskal <yiaswift@gmail.com>
2024-06-13 19:30:21 +0200

    deleted style

--- frontend/src/components/HomePageFinland.jsx
+++ frontend/src/components/HomePageFinland.jsx
@@ -14,25 +14,19 @@ export const HomePageFinland = () => {
       <div className="flex-1 order-2 md:order-1">
         <div className="z-10 relative md:absolute md:top-1/2 md:left-1/2 transform md:-translate-x-2/4 md:-translate-y-1/2 bg-bg1 md:rounded-lg flex flex-col justify-center md:p-12">
           <h2
-            className="lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir text-fontColor md:p-[16px] md:mt-[8px]"
-            style={{ margin: "8px", paddingTop: "8px" }}
+            className="lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir text-fontColor md:p-[16px] md:mt-[8px] m-[8px] pt-[8px]"
           >
             {textTitle}
           </h2>
           <p
-            className="text-fontColor font-avenir lg:text-textlg md:text-textmd sm:text-textsm"
-            style={{
-              margin: "16px",
-              paddingBottom: "8px",
-              paddingRight: "8px",
-            }}
+            className="text-fontColor font-avenir lg:text-textlg md:text-textmd sm:text-textsm m-[16px] pb-[8px] pr-[8px]"
           >
             {text}
           </p>
 
           <Link to={linkTo}>
-            <div style={{ margin: "16px", marginBottom: "50px" }}>
-              <button className="bg-bg2 text-cardBg font-avenir font-bold rounded-lg py-[12px] px-[10px] lg:text-textlg md:text-textmd sm:text-textsm transition-transform duration-300 hover:scale-105">
+            <div className="m-[16px] mb-[50px]">
+              <button className=" bg-bg2 text-cardBg font-avenir font-bold rounded-lg py-[12px] px-[10px] lg:text-textlg md:text-textmd sm:text-textsm transition-transform duration-300 hover:scale-105">
                 {btnName}
               </button>
             </div>
