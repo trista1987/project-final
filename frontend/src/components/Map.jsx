f65b37625edb76d213fb2bd8c7cdafe9959ccd8a
Trista Shan <trista8703@hotmail.com>
2024-06-19 23:23:36 +0300

    change map markicon

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,12 +1,25 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet"
 
 export const Map = ({ position, parkName,className }) => {
+  const markIcon = new L.Icon({
+    iconUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+    shadowSize: [41, 41],
+  })
+
   return (
     <MapContainer
       center={position}
       zoom={13}
       scrollWheelZoom={false}
+      icon={markIcon}
       // style={{ height: "100vh", width: "100%" }}
       className={`${className}`}
     >
