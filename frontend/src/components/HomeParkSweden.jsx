fc53ee02aadcb5f0ffc01971dce8be9e7a962cd0
Citronskal <yiaswift@gmail.com>
2024-06-22 20:16:16 +0200

    accessibility update

--- frontend/src/components/HomeParkSweden.jsx
+++ frontend/src/components/HomeParkSweden.jsx
@@ -5,7 +5,8 @@ export const HomePageSweden = () => {
   const text =
     "Hike beneath the midnight sun in summer, trek through vibrant fall foliage, or chase the Northern Lights in winter. Every season unveils a new adventure in Sweden's breathtaking nature.";
   const textImg = "/backgroundImages/mainpage-sweden.jpg";
-  const textImgAlt = "Sweden Landscape";
+  const textImgAlt =
+    "A scenic landscape of Sweden featuring mountains, forests, and a lake";
   const linkTo = "/sweden";
   const btnName = "Visit Sweden";
 
@@ -19,20 +20,25 @@ export const HomePageSweden = () => {
         />
       </div>
       <div className="flex-1 p-4 md:p-12 md:absolute md:top-1/2 md:right-[-30%] md:w-[50%] md:transform md:-translate-y-1/2 md:-translate-x-3/3">
-        <div className="bg-bg2 md:rounded-lg flex flex-col justify-center p-4 md:p-12">
-          <h2 className="text-cardBg lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir my-4 m-[16px] pt-[8px]">
+        <div
+          className="bg-bg2 md:rounded-lg flex flex-col justify-center p-4 md:p-12"
+          role="region"
+          aria-labelledby="sweden-title"
+        >
+          <h2
+            id="sweden-title"
+            className="text-cardBg lg:text-h2lg md:text-h2md sm:text-h2sm font-avenir my-4 m-[16px] pt-[8px]"
+          >
             {textTitle}
           </h2>
           <p className="text-cardBg lg:text-textlg md:text-textmd sm:text-textsm font-avenir my-4 m-[16px] pb-[8px] pr-[8px]">
             {text}
           </p>
 
-          <Link to={linkTo}>
-            <div className="m-[16px] mb-[50px]">
-              <button className="bg-bg1 text-bg2 font-avenir font-bold rounded-lg md:text-lg py-[12px] px-[10px] transition-transform duration-300 hover:scale-105">
-                {btnName}
-              </button>
-            </div>
+          <Link to={linkTo} className="m-[16px] mb-[50px]">
+            <button className="bg-bg1 text-bg2 font-avenir font-bold rounded-lg md:text-lg py-[12px] px-[10px] transition-transform duration-300 hover:scale-105">
+              {btnName}
+            </button>
           </Link>
         </div>
       </div>
