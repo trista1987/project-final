e94554952dd2279b62f98b93338f599db6fbf557
Citronskal <yiaswift@gmail.com>
2024-06-22 19:38:44 +0200

    button aria-label added

--- frontend/src/components/buttons/LogoutBtn.jsx
+++ frontend/src/components/buttons/LogoutBtn.jsx
@@ -4,6 +4,7 @@ export const LogoutBtn = ({ onclick }) => {
       <button
         className="bg-transparent text-white p-2 font-avenir"
         onClick={onclick}
+        aria-label="Log out of your account"
       >
         Logout
       </button>
