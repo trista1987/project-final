12f109bfad4fb2a7a75dc2283b073843c203ec75
Trista Shan <trista8703@hotmail.com>
2024-06-25 19:35:45 +0300

    added env files

--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -11,8 +11,9 @@ export const Weather = ({ city, timezone, className }) => {
 
   const BASE_URL = "https://api.openweathermap.org/data/2.5/";
   const BASE_URL1 = "weather";
-  const API_KEY = "0a3f5beba05e6db2d5da18ddf3283c92";
-  const URL1 = `${BASE_URL}${BASE_URL1}?q=${city}&units=metric&appid=${API_KEY}`;
+  const apiENV = import.meta.env.VITE_OPENWEATHER_KEY
+  // const API_KEY = "0a3f5beba05e6db2d5da18ddf3283c92";
+  const URL1 = `${BASE_URL}${BASE_URL1}?q=${city}&units=metric&appid=${apiENV}`;
 
   const location = useLocation();
 
